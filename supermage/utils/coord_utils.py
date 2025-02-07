e8acc9ecf038ac0a7dcd61d1a1cf39da7ce0e675
mjyb16 <mjybarth@gmail.com>
2025-02-07 17:05:35 -0500

    Adding ability to shift cube xy and padding the image by zeros, to be tested

--- supermage/utils/coord_utils.py
+++ supermage/utils/coord_utils.py
@@ -54,3 +54,34 @@ def arcsec_to_parsec(distance_mpc, angular_size_arcsec):
     physical_size_pc = distance_pc * angular_size_rad
     
     return physical_size_pc
+
+def parsec_to_arcsec(distance_mpc, physical_size_pc):
+    """
+    Convert physical sizes in parsecs to angular sizes in arcseconds.
+    
+    Parameters:
+    -----------
+    distance_mpc : float or np.ndarray
+        Distance to the galaxy in megaparsecs (Mpc).
+    physical_size_pc : float or np.ndarray
+        Physical sizes in parsecs.
+
+    Returns:
+    --------
+    np.ndarray
+        Angular sizes in arcseconds.
+    """
+    # Convert distance from Mpc to parsecs
+    distance_pc = distance_mpc * 1e6
+    
+    # Using the small-angle approximation:
+    # physical_size_pc = distance_pc * angular_size_rad
+    # => angular_size_rad = physical_size_pc / distance_pc
+    angular_size_rad = physical_size_pc / distance_pc
+    
+    # Convert from radians to arcseconds
+    # 1 radian = (180/pi) degrees, and 1 degree = 3600 arcseconds
+    # so 1 radian = 206265 arcseconds
+    angular_size_arcsec = np.degrees(angular_size_rad) * 3600
+    
+    return angular_size_arcsec
